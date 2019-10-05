28ce69896ddf85f534e9285b84479a951c486993
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-06 02:30:14 +0300

    binary conversion

--- 1st sem/classwork 1 (02.10.19)/Binary (16)/Binary16.java
+++ 1st sem/classwork 1 (02.10.19)/Binary (16)/Binary16.java
@@ -1,7 +1,21 @@
 public class Binary16 {
 	public static void main(String[] args) {
 		if (args.length == 1) {
-			int number = Integer.parse
+			int number = Integer.parseInt(args[0]);
+			int k = 0;
+			int binary[];
+			while (number > 0) {
+				binary.insert(0, number % 2);
+				if (binary[0] == 1) {
+					k++;
+				};
+				number = number / 2;
+				for (i = 0; i < binary.length() - 1; i++) {
+					System.out.print(binary[i]);
+				};
+			 System.out.println(k);
+			};
+
 		} else {
 			System.out.println("Enter one argument");
 		}
