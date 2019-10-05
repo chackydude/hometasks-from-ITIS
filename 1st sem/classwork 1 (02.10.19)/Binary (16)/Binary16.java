d9917525fe8533aae97a7058de36fafaf6b9f70c
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-06 02:06:54 +0300

    System.out.printf()
    task with format of output

---
+++ 1st sem/classwork 1 (02.10.19)/Binary (16)/Binary16.java
@@ -0,0 +1,9 @@
+public class Binary16 {
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			int number = Integer.parse
+		} else {
+			System.out.println("Enter one argument");
+		}
+	}
+}
--- 1st sem/classwork 1 (02.10.19)/Table (12) v2/Table12v2.java
+++ 1st sem/classwork 1 (02.10.19)/Table (12) v2/Table12v2.java
@@ -4,9 +4,11 @@ public class Table12v2 {
 			int n = Integer.parseInt(args[0]);
 			for (int i = 1; i <= 9; i++) {
 				for (int j = 1; j<=n; j++) {
-					int maxMult = j*9 + 1;
-					//add variable to string? replace "10" to "maxMult"
-					System.out.printf("%-10s", j + "*" + i + "=" + i*j);
+					int maxMult = j*9;
+					String maxStr = Integer.toString(maxMult);
+					int spaces = maxStr.length() + 1;
+					System.out.print( j + "*" + i + "=");
+					System.out.printf("%-"+ spaces +"s",i*j);
 			};
 			System.out.printf("%n");
 			};			
