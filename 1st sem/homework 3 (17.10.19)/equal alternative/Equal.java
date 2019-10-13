5514ed05954774aef04439a06304f5364239fa8b
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-13 23:04:48 +0300

    made tasks

---
+++ 1st sem/homework 3 (17.10.19)/equal alternative/Equal.java
@@ -0,0 +1,20 @@
+public class Equal {
+	public static void main(String[] args) {
+		String str1 = args[0];
+		String str2 = args[1];
+		int i = 0;
+		if (str1.length() == str2.length()) {
+			while (str1.charAt(i) == str2.charAt(i)) {
+				i++;
+				if (i == str1.length()) {
+					break;
+				}
+			};
+			if (i == str1.length()) {
+				System.out.println("equals");
+			} else {
+				System.out.println("not equals");
+			}
+		} else System.out.println("not equals");
+	}
+} 
