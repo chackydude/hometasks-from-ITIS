494f021ee13043858e0408fa41f18eb7bbe8c4d9
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-15 01:21:55 +0300

    scanner

---
+++ 1st sem/homework 3 (17.10.19)/compareTo with Scanner/CompareTo.java
@@ -0,0 +1,11 @@
+public class CompareTo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		System.out.println(str1.compareTo(str2));
+	}
+}
+
+
+//scanner can not find symbol
