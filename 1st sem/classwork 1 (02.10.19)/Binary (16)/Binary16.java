03426cdabdacd1c736251f9f2f98dabb31ce52b0
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-16 22:39:53 +0300

    5th task

--- 1st sem/classwork 1 (02.10.19)/Binary (16)/Binary16.java
+++ 1st sem/classwork 1 (02.10.19)/Binary (16)/Binary16.java
@@ -2,19 +2,25 @@ public class Binary16 {
 	public static void main(String[] args) {
 		if (args.length == 1) {
 			int number = Integer.parseInt(args[0]);
-			int k = 0;
-			StringBuffer binary = new StringBuffer("");
-			while (number > 0) {
-				binary.insert(0, number % 2);
-				if (binary.charAt(0) == '1') {
-					k++;
+			if (number >= 0) {
+				int k = 0;
+				StringBuffer binary = new StringBuffer("");
+				while (number > 0) {
+					binary.insert(0, number % 2);
+					if (binary.charAt(0) == '1') {
+						k++;
+					};
+					number = number / 2;
 				};
-				number = number / 2;
-			};
-			for (int i = 0; i <= binary.length() - 1; i++) {
-					System.out.print(binary.charAt(i));
-				}; 
-			System.out.println("\nNumber of units: " + k);
+				for (int i = 0; i <= binary.length() - 1; i++) {
+						System.out.print(binary.charAt(i));
+					}; 
+				System.out.println("\nNumber of units: " + k);
+			} else {
+				//учесть перевод в двоичную СС отрицательноого числа
+			}
+			
+
 
 		} else {
 			System.out.println("Enter one argument");
--- 1st sem/homework 3 (17.10.19)/compareeToIgnoreCase/CompareToIgnore.java
+++ 1st sem/homework 3 (17.10.19)/compareeToIgnoreCase/CompareToIgnore.java
@@ -4,28 +4,31 @@ public class CompareToIgnore {
 		Scanner input = new Scanner(System.in);
 		String str1 = input.nextLine();
 		String str2 = input.nextLine();
-		for (int i = 0; ; i++) {
-			//if two strings are equal
-			if ((i == str1.length()) && (i == str2.length())) {
-				System.out.println(0);
+		int sum1 = 0;
+		int sum2 = 0;
+		int flag = 0;
+		for (int i = 0; i < Math.min (str1.length(), str2.length()); i++){
+
+			if (65 <= (str1.charAt(i)) && (str1.charAt(i) <= 90)) {
+				sum1 = str1.charAt(i) + 32; 
+			} else sum1 = str1.charAt(i);
+
+			if (65 <= (str2.charAt(i)) && (str2.charAt(i) <= 90)) {
+				sum2 = str2.charAt(i) + 32;
+			} else sum2 = str2.charAt(i);
+
+			if (sum1  !=  sum2) {
+				flag = sum1 - sum2;
+				System.out.println(flag);
 				break;
 			};
-			// if strings have different lengths
-			if (str1.length() > str2.length()) {
-				System.out.println(1);
-				break;
-			} else if (str1.length() < str2.length()) {
-				System.out.println(-1);
-				break;
-			}
-			// if strings arn't equals, but have equal lengths
-			// with case ignore
-			if (Math.max(str1.charAt(i), str2.charAt(i)) - 32 != Math. min(str1.charAt(i), str2.charAt(i))) {
-				System.out.println(str1.charAt(i) - str2.charAt(i));
-				break;
-			} else continue;
+		}
+
+		if (flag == 0) {
+			System.out.println(str1.length() - str2.length());
 		}
 	}
 }
+
 
 
