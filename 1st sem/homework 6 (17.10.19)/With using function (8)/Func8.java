01135005624711009a64b9c06e9443f7ba56acad
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-16 23:05:27 +0300

    made 6th task

---
+++ 1st sem/homework 6 (17.10.19)/With using function (8)/Func8.java
@@ -0,0 +1,18 @@
+public class Func8 {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		double x = Double.parseDouble(args[1]);
+		if (args.length == 2 && x>0.0 && n>0) {
+			System.out.println(Count(n, x));
+		} else {
+			System.out.println("Enter two positive arguments");
+		}
+	}
+	public static double Count(int n, double x){
+		double s = 0.0;
+		for (int i = 1; i <= n; i++) {
+			s = x + Math.cos(s);
+		};
+		return s;
+	};
+}
