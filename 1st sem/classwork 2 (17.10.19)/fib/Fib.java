8de7e91ee9b48420c34687f55fef253eeace2314
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-17 17:01:28 +0300

    classwork #2

--- 1st sem/classwork 2 (17.10.19)/fib/Fib.java
+++ 1st sem/classwork 2 (17.10.19)/fib/Fib.java
@@ -2,17 +2,20 @@ public class Fib {
 	public static void main(String[] args) {
 		long n = Long.parseLong(args[0]);
 		System.out.println(calcFib(n));
+		System.out.println(calcFib2(n));
 	}
 	public static long calcFib(long num) {
 		if (num < 0) return 0;
- 		if (num == 0) return 1; 
+ 		if (num == 1) return 1; 
 		return calcFib(num - 2) + calcFib(num - 1);
 	};
 
 	public static long calcFib2(long num) {
 		long fib_sum = 0;
-		long fib1, fib2;
-		while (int i < num - 2) {
+		long fib1 = 1;
+		long fib2 = 1;
+		long i = 0;
+		while (i < num - 2) {
 			fib_sum = fib1 + fib2;
 		    fib1 = fib2;
 		    fib2 = fib_sum;
