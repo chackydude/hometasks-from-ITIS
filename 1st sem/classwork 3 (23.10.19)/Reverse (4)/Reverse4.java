382335a888a0457126cb746247479e4f18a547e5
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-23 20:09:47 +0300

    classwork #3

---
+++ 1st sem/classwork 3 (23.10.19)/Reverse (4)/Reverse4.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Reverse4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+	}
+}
--- 1st sem/classwork 3 (23.10.19)/Sequence (3)/Sequence3.java
+++ 1st sem/classwork 3 (23.10.19)/Sequence (3)/Sequence3.java
@@ -3,7 +3,15 @@ public class Sequence3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int k = input.nextInt();
-		double x = input.nextDouble();
+		while (k <= 0) {
+			System.out.println("enter the positive number of members");
+			k = input.nextInt();
+		};
+		int x = input.nextInt();
+		while (x == 3) {
+			System.out.println("invalid value, x != 3");
+			x = input.nextInt();
+		};
 		double sum = 0;
 
 		for (int i = 1; i < k*2; i = i + 2) {
