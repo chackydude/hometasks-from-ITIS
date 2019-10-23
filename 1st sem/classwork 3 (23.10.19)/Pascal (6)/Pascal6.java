11e565c9a42510f2f63c1f17805398de2b8b8345
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-23 22:26:40 +0300

    classwork #3

--- 1st sem/classwork 3 (23.10.19)/Pascal (6)/Pascal6.java
+++ 1st sem/classwork 3 (23.10.19)/Pascal (6)/Pascal6.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class Pascal6 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		int height = input.nextInt();
+		for (int i = 0; i <= k-1; i++) {
+
+		}
 	}
 }
