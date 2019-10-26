c6040fd59d3ee9262917ec79fd87a3458dd501a7
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-26 19:03:52 +0300

    tringle of Pascal (classwork #4)

--- 1st sem/classwork 3 (23.10.19)/Pascal (6)/Pascal6.java
+++ 1st sem/classwork 3 (23.10.19)/Pascal (6)/Pascal6.java
@@ -2,46 +2,49 @@ import java.util.Scanner;
 public class Pascal6 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int height = input.nextInt();
+		long height = input.nextInt();
 
-		int maxLen = calcLen(height); // определяем длину наибольшей строки
+		long maxLen = calcLen(height); // определяем длину наибольшей строки
 
-		for (int i = 0; i <= height; i++) {
-			for (int g = 0; g < (maxLen - calcLen(g))/2; g++) {
-				System.out.print(calcLen(g));
-				System.out.print('*');
+		for (int i = 0; i < height; i++) {
+			for (int g = 0; g < Math.ceil((maxLen - calcLen(i))/2); g++) {
+				System.out.print(' ');
 			};
 
 			for (int j = 0; j <= i; j++) {
 				System.out.print(calcFact(i) / (calcFact(j)*calcFact(i-j))); // печать числа в строке треугольника Паскаля
 				System.out.print(' '); // пробел после числа
 			};
-
 			System.out.println(); //перевод на новую строку
 		};
+
 	};
 
-	// вычисление факториаоа
-	public static int calcFact(int num) {
-		if (num < 0) return -1;
- 		if (num == 0) return 1; 
-		return num * calcFact(num-1);
+	// вычисление факториала
+	public static long calcFact(long num) {
+		long f = 1;
+		for (long i = num; i > 0; i--) {
+			f = f * i;
+		};
+		return f;
 	};
 	// вычисление количсетва символов в строке
-	public static int calcLen(int num) {
-		int sumDigit = 0;
-		for (int j = 0; j < num; j++) {
-		int countDigit = 0;
-		double number = calcFact(num-1) / (calcFact(j)*calcFact(num-1-j)); 
-				do {
-					number = number / 10;
-					countDigit++;
-				} while (number > 9);
-				sumDigit = sumDigit + countDigit;
+	public static long calcLen(long num) {
+		long sumDigit = 0; // переменная хранящая текущую длину текущей строки (точлько чисел)
+		for (long j = 0; j <= num; j++) {
+		long countDigit = 0;// переменная хранящая количество цифр текущей строки
+		long number = calcFact(num) / (calcFact(j)*calcFact(num-j)); 
+		sumDigit = sumDigit + lengthOfNumber(number);
 		};
-		return sumDigit;
+		return sumDigit + num - 1; // кол-во символов в строке с учетом пробелов
+	}
+	//вычисление длины числа
+	public static long lengthOfNumber(long number) {
+		long countDigit = 0;
+		while (number > 0) {
+			number = number / 10;
+			countDigit++;
+		};
+		return countDigit;
 	}
 }
-
-// TODO количсетво пробелов перед строкой - это разность количества символов 
-// максимальной строки и итерируемой строки деленная на 2, т.е можно попробовать вычислять длину каждой строки
