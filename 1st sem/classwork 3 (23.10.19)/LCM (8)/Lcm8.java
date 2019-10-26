632e18f34433bfef5578b3c201d16c745187e997
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-27 02:13:20 +0300

    classworl #4

--- 1st sem/classwork 3 (23.10.19)/LCM (8)/Lcm8.java
+++ 1st sem/classwork 3 (23.10.19)/LCM (8)/Lcm8.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
+//наименьшее общее кратное - наименьшее число, которе делится на оба без остатка
 public class Lcm8 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int number1 = input.nextInt();
 		int number2 = input.nextInt();
+		boolean flag = true;
+		int result = Math.min(number1, number2);
 
-		for (int i = 2; i < Math.min(number1, number2); i++) {
-			if (number1 % i == 0 && number2 % i == 0) {
-				System.out.printf("\nLCM is - %d", i);
-			};
+		while (flag) {
+			if ((result % number1 == 0) && (result % number2 == 0)) {
+				flag = false;
+				System.out.println(result);	
+			} else result++;
 		};
 	}
 }
