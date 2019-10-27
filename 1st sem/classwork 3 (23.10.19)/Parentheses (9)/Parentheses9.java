44c884878b00ea4c572d40a0b7044285f0624eca
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-27 21:55:51 +0300

    classwork #4
    reqursion - huyoursion

--- 1st sem/classwork 3 (23.10.19)/Parentheses (9)/Parentheses9.java
+++ 1st sem/classwork 3 (23.10.19)/Parentheses (9)/Parentheses9.java
@@ -3,21 +3,53 @@ public class Parentheses9 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String str = input.nextLine();
+		boolean flag = true;
 
-		// проверка скобок нерекурсивно
-		int count1 = 0, count2 = 0;
+		// // проверка скобок нерекурсивно
+		// int count1 = 0, count2 = 0;
+
+		// for (int i = 0; i < str.length(); i++) {
+		// 	switch (str.charAt(i)) {
+		// 		case ('('): 
+		// 			count1++;
+		// 			break;
+		// 		case (')'):
+		// 			count2++;
+		// 			break; 
+		// 	};
+
+		// };
+		// System.out.println(count1 == count2);
 
 		for (int i = 0; i < str.length(); i++) {
-			switch (str.charAt(i)) {
-				case ('('): 
-					count1++;
-					break;
-				case (')'):
-					count2++;
-					break; 
-			};
-
+			if (findParenthese(i, str)) {
+				continue;
+			} else {
+				flag = false;
+				System.out.println(flag);
+				break;
+			}
 		};
-		System.out.println(count1 == count2);
+		if (flag) {
+			System.out.println(flag);
+		};
+	}
+
+	// функция поиска споряженных скобок, нашли - true, иначе - false
+	public static boolean findParenthese(int i, String str) {
+		for (int j = i; j < str.length(); j++) {
+			if (str.charAt(i) == '(') {
+				switch (str.charAt(j)) {
+					case ('('): 
+						return findParenthese(j, str);
+						break;
+					case (')'):
+						return true;
+						break;
+					default: 
+						return false;
+				};
+			};
+		};
 	}
 }
