022eacedd8a9fb022b6bc202613db333435f0d00
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-28 18:21:31 +0300

    classwork #4

--- 1st sem/classwork 3 (23.10.19)/Parentheses (9)/Parentheses9.java
+++ 1st sem/classwork 3 (23.10.19)/Parentheses (9)/Parentheses9.java
@@ -3,41 +3,35 @@ public class Parentheses9 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String str = input.nextLine();
-		boolean flag = true;
+		boolean flag = true, flag2 = false;
 
-		// // проверка скобок нерекурсивно
-		// int count1 = 0, count2 = 0;
-
-		// for (int i = 0; i < str.length(); i++) {
-		// 	switch (str.charAt(i)) {
-		// 		case ('('): 
-		// 			count1++;
-		// 			break;
-		// 		case (')'):
-		// 			count2++;
-		// 			break; 
-		// 	};
-
-		// };
-		// System.out.println(count1 == count2);
 		for (int i = 0; i < str.length(); i++) {
-			
+			if (findParenthese2(i, str) == -1) {
+				System.out.println(false);
+				break;
+			}
 		}
-	}
 
-	// функция поиска споряженных скобок, нашли - true, иначе - false
-	public static int findParenthese(int i, String str) {
-		for (int j = i; j < str.length(); j++) {
-				switch (str.charAt(j)) {
+}
+
+// функция, которая ищет для каждой открывающей скобки '(' парную закрывающую ')'
+// возвращает саму себя, если найдена еще одна открывающая скобка '('
+// возвращает позицию следующую после найденной	парной закрывающей скобки
+// возвразщает -1 если парная скобка не найдена
+	public static int findParenthese2(int i, String str) {
+		int output = -1;
+		if (str.charAt(i) == '(') {
+			for (int k = i; k < str.length(); k++) {
+				switch (str.charAt(k)) {
 					case ('('): 
-						return findParenthese(j, str);
-						break;
+						output = findParenthese2(i+1, str);
 					case (')'):
-						return j;
-						break;
-					default: 
-						return -1;
+						output = k+1;
+					default:
+						output = -1;
+				};
 			};
-		};
-	}
+	 	 };
+ 		return output;
+  }
 }
