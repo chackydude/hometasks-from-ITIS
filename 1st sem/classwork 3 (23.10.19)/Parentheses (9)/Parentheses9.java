6f0795da195a48a04a8e41ced21fb8a1f9c26ebf
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-10-29 08:09:42 +0300

    classwork #4

--- 1st sem/classwork 3 (23.10.19)/Parentheses (9)/Parentheses9.java
+++ 1st sem/classwork 3 (23.10.19)/Parentheses (9)/Parentheses9.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 public class Parentheses9 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String str = input.nextLine();
-		boolean flag = true, flag2 = false;
+		String strInput = input.nextLine();
+		StringBuilder str = new StringBuilder(strInput);
 
+		boolean flag = true;
+		// проходим по всей строке
 		for (int i = 0; i < str.length(); i++) {
+			// если не найдено парной для последней '(' закрывающей ')'
 			if (findParenthese2(i, str) == -1) {
-				System.out.println(false);
 				flag = false;
+				System.out.println(flag);
 				break;
+			// если нашли закрывающую ')', о удаляем часть строки от последней '(' до ')'
 			} else i = findParenthese2(i, str);
 		};
+	 // если есть какое-то несоответствие в строке
 	 if (flag) {
 	 	System.out.println(flag);
 	 };
@@ -22,20 +27,28 @@ public class Parentheses9 {
 // возвращает позицию следующую после найденной	парной закрывающей скобки
 // возвразщает -1 если парная скобка не найдена
 
-	public static int findParenthese2(int i, String str) {
+	public static int findParenthese2(int i, StringBuilder str) {
 		int output = -1;
+		// поиск первого символа '(' 
 		if (str.charAt(i) == '(') {
 			for (int k = i; k < str.length(); k++) {
 				switch (str.charAt(k)) {
+					//при нахождении еще одной '(' вызываем функцию опять
 					case ('('): 
-						output = findParenthese2(i+1, str);
+						output = findParenthese2(k, str);
+						break;
+					//при нахождении ')' удаляем часть строки от последней '(' до первой ')'
 					case (')'):
-						output = k+1;
+						str.delete(i, k+1);
+						output = i+1;
+						break;
+					//если к '(' е найдено парной ')', то вывод -1
 					default:
 						output = -1;
 				};
 			};
 	 	 };
+
  		return output;
   }
 }
