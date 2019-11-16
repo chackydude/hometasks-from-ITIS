7ba69aec48daa9d6a2cf9f307ddfb0d594b39a40
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-11-17 01:22:45 +0300

    homework #7
    pascal

--- 1st sem/homework 7 (20.11.19)/All tasks for 7th homework/src/Pascal.java
+++ 1st sem/homework 7 (20.11.19)/All tasks for 7th homework/src/Pascal.java
@@ -2,32 +2,32 @@ import java.util.Scanner;
 public class Pascal {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter height");
+        System.out.println("Enter height of Pascal's triangle");
         long height = input.nextInt();
 
-        //length of the max array
+        /* length of the max array */
         long maxLen = calcLen(height);
 
         for (int i = 0; i < height; i++) {
             for (int g = 0; g < Math.ceil((maxLen - calcLen(i))/2); g++) {
                 System.out.print(' ');
             };
-            //initialization of the max array with length which equals to the number of the Pascal's triangle string
+            /* initialization of the max array with length which equals to the number of the Pascal's triangle string */
             int[] array = new int[i+1];
             for (int j = 0; j <= i; j++) {
                 array[j] = (int) (calcFact(i) / (calcFact(j)*calcFact(i-j)));
-                //print number of iterable string
+                /* print number of iterable string */
                 System.out.print(array[j]);
-                //gap between two numbers
+                /* gap between two numbers */
                 System.out.print(' ');
             };
-            //go to thw new string
+            /* go to thw new string */
             System.out.println();
         };
 
     };
 
-    //calculating factorial
+    /* calculating factorial */
     public static long calcFact(long num) {
         long f = 1;
         for (long i = num; i > 0; i--) {
@@ -35,7 +35,7 @@ public class Pascal {
         };
         return f;
     };
-    //calculating quantity of the number in the string
+    /* calculating quantity of the number in the string */
     public static long calcLen(long num) {
         // variable what
         long sumDigit = 0;
@@ -43,9 +43,10 @@ public class Pascal {
             long number = calcFact(num) / (calcFact(j)*calcFact(num-j));
             sumDigit = sumDigit + lengthOfNumber(number);
         };
-        return sumDigit + num - 1; // кол-во символов в строке с учетом пробелов
+        /* quantity of the numbers and spases in the string */
+        return sumDigit + num - 1;
     }
-    //вычисление длины числа
+    /*calculate length of the number*/
     public static long lengthOfNumber(long number) {
         long countDigit = 0;
         while (number > 0) {
