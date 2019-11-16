d2a08dfb54c6e16daec384c4273e9981c3b97379
Daniil Trofimov <trofimovdanil946@gmail.com>
2019-11-16 21:54:32 +0300

    homework #7
    copy

---
+++ 1st sem/homework 7 (20.11.19)/All tasks for 7th homework/src/Copy.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Copy {
+    public static void main(String[] args) {
+        System.out.println("Enter array's length");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] array  = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.println("Even array");
+        if (copyArray(array).length == 0) {
+            System.out.println("Empty");
+        } else {
+            System.out.println(Arrays.toString(copyArray(array)));
+        }
+    }
+
+    public static int[] copyArray(int[] array) {
+        int copyLength = 0;
+        for (int i = 0; i < array.length; i = i + 2) {
+            if (array[i] % 2 == 0) {
+                copyLength++;
+            }
+        }
+        int[] copy = new int[copyLength];
+        int pos = 0;
+        for (int i = 0; i < array.length; i = i + 2) {
+            if (array[i] % 2 == 0) {
+                copy[pos]  = array[i];
+                pos++;
+            }
+        }
+        return copy;
+    }
+}
